c371b3a203492c72c407bb65e0e8de63f665b06b
Alex Kademan <alexkademan@gmail.com>
2016-07-31 14:44:50 -0500

    something keeps allowing me to commit the mb-config file to GITHUB,... so I keep having to reset my confidential info.

--- MINDBODY/mb-config.php
+++ MINDBODY/mb-config.php
@@ -2,6 +2,6 @@
 
 $mb_config = array(
 	'SourceName' => 'HomeOffice',
-	'Password' => '+XQbNG6zAr9IRi8CxCZRBqbd1RU=',
+	'Password' => '+XQbNG6zAr9IRi8CxCZRBqbd1RU=', // why does this keep committing to GITHUB !@?!
 	'SiteIDs' => array('44288')
 );
