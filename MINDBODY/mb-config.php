d884dd364295e9a630cdac87400b3ae0f3b136d2
Alex Kademan <alexkademan@gmail.com>
2016-07-31 15:00:59 -0500

    finally learned to remove the config file after accidentally committing the thing

--- MINDBODY/mb-config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$mb_config = array(
-	'SourceName' => 'HomeOffice', 
-	'Password' => '+XQbNG6zAr9IRi8CxCZRBqbd1RU=', // why does this keep committing to GITHUB !@?!
-	'SiteIDs' => array('44288')
-);
