5b3e3d21a2fd6574976731268828c1b71ade456f
Alex Kademan <alexkademan@gmail.com>
2016-07-31 14:54:52 -0500

    fighting with .gitignore

--- MINDBODY/mb-config.php
+++ MINDBODY/mb-config.php
@@ -1,7 +1,7 @@
 <?php
 
 $mb_config = array(
-	'SourceName' => 'HomeOffice',
+	'SourceName' => 'HomeOffice', // moar changes !@#!
 	'Password' => '+XQbNG6zAr9IRi8CxCZRBqbd1RU=', // why does this keep committing to GITHUB !@?!
 	'SiteIDs' => array('44288')
 );
